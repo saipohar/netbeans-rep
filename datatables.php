223321c6c47e69d1d080e2a7af5779ee3eec2fe0
Saikumar <saipohar@gmail.com>
2018-06-20 16:47:26 +0530

    first-commit

---
+++ datatables.php
@@ -0,0 +1,62 @@
+<html>
+    <head>
+        <title>data table</title>
+        <script src="jquery-latest.js"></script>
+        <script src="jquery.tablesorter.js"></script>
+    </head>
+    <body>
+        <div class="data-tables">
+            <table>
+                <thead>
+                    <tr>
+                        <th>s.no</th>
+                        <th>name</th>
+                        <th>number</th>
+                        <th>email</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>1</td>
+                        <td>saikumar</td>
+                        <td>979797977</td>
+                        <td>saikumar@gmail.com</td>
+                    </tr>
+                    <tr>
+                        <td>2</td>
+                        <td>rajkumar</td>
+                        <td>7898789887</td>
+                        <td>rajkumar@gmail.com</td>
+                    </tr>
+                    <tr>
+                        <td>3</td>
+                        <td>shivkumar</td>
+                        <td>578487985</td>
+                        <td>shickumar@gmail.com</td>
+                    </tr>
+                    <tr>
+                        <td>4</td>
+                        <td>arunkumar</td>
+                        <td>857968958</td>
+                        <td>arunkumar@gmail.com</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </body>
+</html>
+
+<style>
+    table, tr, tbody, thead, th, td{
+        border: 1px solid #cccccc;
+        padding: 5px 10px;
+        text-align: center;
+    }
+</style>
+
+<script>
+    $(document).ready(function () {
+        // call the tablesorter plugin, the magic happens in the markup
+        $("table").tablesorter();
+    });
+</script>
